8bae3e1100f6b284a33a0359e6b7cb305cb2b2da
Joost Ole Seddig <jst_sddg@outlook.com>
2021-03-28 23:34:24 +0200

    Implemented caching for Alpha Beta Pruning

--- src/alpha_beta_pruning.rs
+++ src/alpha_beta_pruning.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{heuristic::HeuristicWeights, player::Player, state::State};
 
 
@@ -10,6 +12,7 @@ pub struct AlphaBetaPruningResult {
 
 #[derive(Debug, Clone)]
 pub struct AlphaBetaPruningOptions {
+    pub cache: bool,
     pub limit: u8,
     pub weights: HeuristicWeights,
 }
@@ -17,6 +20,7 @@ pub struct AlphaBetaPruningOptions {
 impl Default for AlphaBetaPruningOptions {
     fn default() -> Self {
         AlphaBetaPruningOptions {
+            cache: true,
             limit: 3,
             weights: Default::default(),
         }
@@ -24,20 +28,63 @@ impl Default for AlphaBetaPruningOptions {
 }
 
 pub struct AlphaBetaPruning {
+    cache: HashMap<(State, Player, u8), (i16, i16, i16)>,
     visited: u32,
     options: AlphaBetaPruningOptions,
+}
+
+enum CacheHit {
+    Valid { value: i16 },
+    Invalid { alpha: i16, beta: i16 },
 }
 
 impl AlphaBetaPruning {
     pub fn new(options: AlphaBetaPruningOptions) -> Self {
         AlphaBetaPruning {
+            cache: HashMap::new(),
             visited: 0,
             options: options
         }
     }
 
-    fn value(&mut self, state: &State, player: Player, mut alpha: i16, beta: i16, limit: u8) -> i16 {
+    fn get_cache(&self, state: &State, player: Player, alpha: i16, beta: i16, limit: u8) -> Option<CacheHit> {
+        if !self.options.cache {
+            return None
+        }
+
+        match self.cache.get(&(state.clone(), player, limit)) {
+            Some((value, cache_alpha, cache_beta)) => {
+                Some(if cache_alpha <= &alpha && &beta <= cache_beta {
+                    CacheHit::Valid { value: *value }
+                } else {
+                    CacheHit::Invalid {
+                        alpha: alpha.min(*cache_alpha),
+                        beta: beta.max(*cache_beta),
+                    }
+                })
+            }
+            None => None,
+        }
+    }
+
+    fn set_cache(&mut self, state: &State, player: Player, limit: u8, value: i16, alpha: i16, beta: i16) {
+        if self.options.cache {
+            self.cache.insert((state.clone(), player, limit), (value, alpha, beta));
+        }
+    }
+
+    fn value(&mut self, state: &State, player: Player, mut alpha: i16, mut beta: i16, limit: u8) -> i16 {
         self.visited += 1;
+
+        if let Some(cache) = self.get_cache(state, player, alpha, beta, limit) {
+            match cache {
+                CacheHit::Valid { value} => return value,
+                CacheHit::Invalid { alpha: cache_alpha, beta: cache_beta } => {
+                    alpha = cache_alpha;
+                    beta = cache_beta;
+                }
+            }
+        }
         
         if state.finished(player) {
             return state.utility(player);
@@ -53,11 +100,13 @@ impl AlphaBetaPruning {
             );
 
             if value >= beta {
+                self.set_cache(state, player, limit, value, alpha, beta);
                 return value;
             }
             alpha = value.max(alpha);
         }
 
+        self.set_cache(state, player, limit, value, alpha, beta);
         value
     }
 
